d29040e190afe8e642e0a9438533f9baf5a7541b
unknown <Vidyadhar S P>
2023-07-10 13:34:23 +0530

    student-management

---
+++ src/test/java/com/vidyadhar/studentmanagement1/StudentManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidyadhar.studentmanagement1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
